290a965d0db7cae801f0a0b59e8976a9e5dea0bf
rose hall <behindthewallofnoise@gmail.com>
2020-03-10 09:55:20 -0700

    created main.cpp file. added a bit of psuedocode to organize my thoughts and show order in which i will do things.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+// name: rose hall
+// date: 03.xx.20
+// shunting yard algorithm.
+
+// node struct for stack and queue linked lists.
+
+
+int main() {
+  // base variables
+  char* eqInput = new char[200];
+  char* transInput = new char[8];
+  
+ 
+  // instructions on how to input the equation
+  cout << "this is where the instructions for equation input will go." << endl;
+  // always input in infix, spaces between every token (tokens being numbers and operators)
+  
+  // take input for equation
+  
+  // parse input into numbers and operators
+
+  // [THIS IS THE SHUNTING YARD ALGORITHM]
+  // place numbers into queue, place operators into stack, rearrange them
+  // postfix will eventually be in the queue
+
+  // use postfix to build the tree
+  
+  // while loop start
+
+  // instructions on how to use the translator
+  cout << "this is where the instructions for translator use will go." << endl;
+
+  // take input for translation
+
+  // perform translation
+
+  // output translation
+
+  // while loop end, there will be a quit function.
+
+
+}
